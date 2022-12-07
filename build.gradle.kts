a35954037f89d3d7bd66597ce1832f94ce3415ea
WhoIsWoony <WhoIsWoony@gmail.com>
2022-12-08 02:01:31 +0900

    feat: apply Swagger

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.7.6"
 	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+	id("org.springdoc.openapi-gradle-plugin") version "1.5.0"	//Swagger
+
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
 
@@ -10,6 +12,7 @@ plugins {
 	kotlin("plugin.allopen") version "1.6.21" // allOpen에서 지정한 어노테이션으로 만든 클래스에 open 키워드 적용
 	// - Hibernate가 사용하는 Reflection API에서 Entity를 만들기 위해 인자 없는 기본 생성자가 필요함
 	kotlin("plugin.noarg") version "1.6.21" // 인자 없는 기본 생성자를 자동 생성
+
 }
 
 allOpen {
@@ -31,19 +34,24 @@ repositories {
 }
 
 dependencies {
-	//기본세팅
+	//Spring-Boot
 	implementation("org.springframework.boot:spring-boot-starter") // Spring boot 를 이용하기 위한 위존성
 	implementation("org.springframework.boot:spring-boot-starter-web") // Spring boot web과 관련 controller 작성을 위함
 	implementation("org.jetbrains.kotlin:kotlin-reflect") // Kotlin 런타임 라이브러리 용량을 줄이기 위한 위존성
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8") // Kotlin 필수 기능, let, apply, use, synchronized 등
 
+	//JPA = MariaDB
 	runtimeOnly("org.mariadb.jdbc:mariadb-java-client") //Kotlin과 Mariadb을 연결
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa") // JPA - Hibernate 의존성 설정
 	implementation("org.jetbrains.kotlin:kotlin-allopen") //Kotlin에서 Hibernate를 원할하게 사용하기 위함
 	implementation("org.jetbrains.kotlin:kotlin-noarg") //Kotlin에서 Hibernate를 원할하게 사용하기 위함
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin") //Json 라이브러리
 
-	//JUnit 테스트
+	//Swagger = springdoc-openapi
+	implementation("org.springdoc:springdoc-openapi-ui:1.6.13")
+	implementation("org.springdoc:springdoc-openapi-kotlin:1.6.13")
+
+	//JUnit = Kotest + mockk
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.mockk:mockk:1.13.2")
 	testImplementation("io.kotest:kotest-runner-junit5:5.5.4")
--- src/main/kotlin/com/whoiswoony/springtutorial/controller/TodoApi.kt
+++ src/main/kotlin/com/whoiswoony/springtutorial/controller/TodoApi.kt
@@ -1,13 +1,17 @@
 package com.whoiswoony.springtutorial.controller
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@Tag(name = "투두를 만들건데요 삽질 중 입니다.")
 @RestController
 @RequestMapping("/")
 class TodoApi {
 
+    @Operation(summary = "인사하는 Hello Todo")
     @GetMapping("/")
     fun helloTodo(): String {
         return "Hello Todo"
