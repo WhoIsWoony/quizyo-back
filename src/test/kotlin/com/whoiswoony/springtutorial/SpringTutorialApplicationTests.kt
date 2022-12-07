3f5a148788dcd98f3a50bc685392d71233c9048f
WhoIsWoony <WhoIsWoony@gmail.com>
2022-12-07 20:28:50 +0900

    feat: init to sprint boot 2.7.6

---
+++ src/test/kotlin/com/whoiswoony/springtutorial/SpringTutorialApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.whoiswoony.springtutorial
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringTutorialApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
