d9f9a7d4077fb832cdc9256912add7e3c88be6d6
WhoIsWoony <WhoIsWoony@gmail.com>
2022-12-09 01:45:35 +0900

    chore: change api tag name

--- src/main/kotlin/com/whoiswoony/springtutorial/controller/Space.kt
+++ src/main/kotlin/com/whoiswoony/springtutorial/controller/Space.kt
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
-@Tag(name = "QuizS : Auth API")
+@Tag(name = "QuizS : Space API")
 @RestController
 @RequestMapping("/")
 class Space(private val todoService: TodoService) {
