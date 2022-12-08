dffc880951e24de40a15e3791eba863d7067af83
WhoIsWoony <WhoIsWoony@gmail.com>
2022-12-09 01:49:56 +0900

    feat: Quiz and Set Controller

---
+++ src/main/kotlin/com/whoiswoony/springtutorial/controller/Set.kt
@@ -0,0 +1,25 @@
+package com.whoiswoony.springtutorial.controller
+
+import com.whoiswoony.springtutorial.service.TodoService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.math.BigInteger
+
+@Tag(name = "QuizS : Space API")
+@RestController
+@RequestMapping("/")
+class Set(private val todoService: TodoService) {
+
+    @Operation(summary = "그룹내퀴즈들불러오기")
+    @GetMapping("/findSetBySpaceId")
+    fun findSetBySpaceId(
+        @RequestParam accessToken:String,
+        @RequestParam spaceId:BigInteger,
+    ): Boolean {
+        return true
+    }
+}
