25671cdd72c1de93d396a84dfad79af272b9f18d
WhoIsWoony <WhoIsWoony@gmail.com>
2023-02-10 13:33:17 +0900

    feat: add corsconfig

--- src/main/kotlin/com/whoiswoony/springtutorial/config/cors/CorsConfig.kt
+++ src/main/kotlin/com/whoiswoony/springtutorial/config/cors/CorsConfig.kt
@@ -1,4 +1,22 @@
 package com.whoiswoony.springtutorial.config.cors
 
-class CorsConfig {
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfig : WebMvcConfigurer {
+
+    override
+    fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000","https://quizyo.develop.woonyhouse.com")
+            .allowedMethods(
+                HttpMethod.GET.name,
+                HttpMethod.POST.name,
+                HttpMethod.PUT.name,
+                HttpMethod.DELETE.name
+            )
+    }
 }
